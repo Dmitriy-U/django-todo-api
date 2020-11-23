ca36ba9c1174a836b2579cb04ce77a508b31da1a
Dmitriy <dmitriyushkalov@gmail.com>
2020-11-23 21:53:50 +0300

    Added new project - todo

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'done',)
+    list_display_links = ('id', 'title',)
+    list_editable = ('done',)
+    list_filter = ('done',)
+    search_fields = ('id', 'title', 'description',)
+
+
+admin.site.register(Todo, TodoAdmin)
